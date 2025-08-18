200561d949f9b5af0cf1fd9cda3db6a3ceae3f3c
Henry Kupty <hkupty@gmail.com>
2025-08-18 22:20:42 +0200

    refactor: Parse CLI arguments
    So it is configurable and not only harcoded with my standards

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,47 @@
 const std = @import("std");
 const walker = @import("walker.zig");
 
-// TODO: Add clap
-//zig fetch --save git+https://github.com/Hejsil/zig-clap
-
-// TODO: Make it configurable
 const variants = [_][]const u8{ "TODO", "HACK", "FIX", "FIXME" };
 
-// TODO: Make it configurable
 // TODO: Enable different comment prefixes for different file types
 const extensions = [_][]const u8{
-    ".zig",
-    ".kt",
-    ".java",
-    ".go",
+    "zig",
+    "kt",
+    "java",
+    "go",
+};
+
+const stdout = std.io.getStdOut();
+
+const help =
+    \\todo-zen [options]
+    \\
+    \\Options:
+    \\     -h --help            Shows this help text
+    \\     -d --max-depth       Maximum traversal depth. Set to 0 to disable            [default: 8]
+    \\     -D --max-src-depth   Maximum depth for a `src/` folder. Set to 0 to disable  [default: 3]
+    \\     -m --markers         Comment markers to look for in the comments.            [default: TODO,HACK,FIX,FIXME]
+    \\     -x --extensions      File extensions to be considered during search.         [default: zig,java,kt,go]
+    \\
+;
+
+const Config = struct {
+    markers: [][]const u8,
+    maxDepth: usize,
+    maxSrcDepth: usize,
+    fileTypes: [][]const u8,
+
+    pub fn deinit(self: *const Config, allocator: std.mem.Allocator) void {
+        for (self.markers) |marker| {
+            allocator.free(marker);
+        }
+
+        for (self.fileTypes) |ft| {
+            allocator.free(ft);
+        }
+        allocator.free(self.markers);
+        allocator.free(self.fileTypes);
+    }
 };
 
 const Match = struct {
@@ -63,9 +91,9 @@ const TodoIterator = struct {
         return null;
     }
 
-    fn readTODOComments(self: *TodoIterator) !?Match {
+    fn readTODOComments(self: *TodoIterator, config: Config) !?Match {
         while (try self.readCommentsDirect()) |comment| {
-            for (variants) |prefix| {
+            for (config.markers) |prefix| {
                 if (std.mem.indexOf(u8, comment.text, prefix)) |index| {
                     return .{
                         .text = comment.text[index..],
@@ -79,19 +107,19 @@ const TodoIterator = struct {
     }
 };
 
-fn readTodos(allocator: std.mem.Allocator, identifier: []const u8, file: std.fs.File) !void {
+fn readTodos(allocator: std.mem.Allocator, identifier: []const u8, file: std.fs.File, config: Config) !void {
     var iterator = TodoIterator{
         .reader = file.reader(),
         .allocator = allocator,
         .commentBuffer = std.ArrayList(u8).init(allocator),
     };
-    const stdout = std.io.getStdOut();
+
     var lineBuilder = try std.ArrayList(u8).initCapacity(allocator, identifier.len + 100);
 
     lineBuilder.appendSliceAssumeCapacity(identifier);
     lineBuilder.appendAssumeCapacity(':');
 
-    while (try iterator.readTODOComments()) |line| {
+    while (try iterator.readTODOComments(config)) |line| {
         lineBuilder.shrinkRetainingCapacity(identifier.len + 1);
         const linenumber = try std.fmt.allocPrint(allocator, "{d}", .{line.linenumber});
         const column = try std.fmt.allocPrint(allocator, "{d}", .{line.column});
@@ -107,6 +135,88 @@ fn readTodos(allocator: std.mem.Allocator, identifier: []const u8, file: std.fs.
     }
 }
 
+pub fn parseConfigFromArgs(allocator: std.mem.Allocator) !Config {
+    var args = std.process.args();
+    defer args.deinit();
+    _ = args.skip();
+
+    var cursor: ?[]const u8 = null;
+    var markers: ?[][]const u8 = null;
+    var filetypes: ?[][]const u8 = null;
+    var maxDepth: ?usize = null;
+    var maxSrcDepth: ?usize = null;
+    while (args.next()) |arg| {
+        if (cursor) |flag| {
+            if (std.mem.eql(u8, "-m", flag) or std.mem.eql(u8, "--markers", flag)) {
+                const max = std.mem.count(u8, arg, ",") + 1;
+                markers = try allocator.alloc([]const u8, max);
+
+                var splitIter = std.mem.splitScalar(u8, arg, ',');
+                var ix: usize = 0;
+                while (splitIter.next()) |nxt| : (ix += 1) {
+                    const marker = allocator.dupe(u8, nxt) catch |err| {
+                        std.log.err(
+                            "Failed to allocate more memory. Will try to operate on best effort. {any}",
+                            .{err},
+                        );
+                        break;
+                    };
+                    markers.?[ix] = marker;
+                }
+            } else if (std.mem.eql(u8, "-x", flag) or std.mem.eql(u8, "--extensions", flag)) {
+                const max = std.mem.count(u8, arg, ",") + 1;
+                filetypes = try allocator.alloc([]const u8, max);
+
+                var splitIter = std.mem.splitScalar(u8, arg, ',');
+                var ix: usize = 0;
+                while (splitIter.next()) |nxt| : (ix += 1) {
+                    const ft = allocator.dupe(u8, nxt) catch |err| {
+                        std.log.err(
+                            "Failed to allocate more memory. Will try to operate on best effort. {any}",
+                            .{err},
+                        );
+                        break;
+                    };
+                    filetypes.?[ix] = ft;
+                }
+            } else if (std.mem.eql(u8, "-d", flag) or std.mem.eql(u8, "--max-depth", flag)) {
+                maxDepth = try std.fmt.parseInt(usize, arg, 10);
+            } else if (std.mem.eql(u8, "-D", flag) or std.mem.eql(u8, "--max-src-depth", flag)) {
+                maxSrcDepth = try std.fmt.parseInt(usize, arg, 10);
+            }
+            cursor = null;
+        } else {
+            if (std.mem.eql(u8, "-h", arg) or std.mem.eql(u8, "--help", arg)) {
+                _ = try stdout.writeAll(help);
+                std.process.exit(0);
+            }
+
+            cursor = arg;
+        }
+    }
+
+    if (markers == null) {
+        markers = try allocator.alloc([]const u8, variants.len);
+        for (variants, 0..) |marker, ix| {
+            markers.?[ix] = try allocator.dupe(u8, marker);
+        }
+    }
+
+    if (filetypes == null) {
+        filetypes = try allocator.alloc([]const u8, extensions.len);
+        for (extensions, 0..) |ft, ix| {
+            filetypes.?[ix] = try allocator.dupe(u8, ft);
+        }
+    }
+
+    return .{
+        .markers = markers.?,
+        .fileTypes = filetypes.?,
+        .maxDepth = maxDepth orelse 8,
+        .maxSrcDepth = maxSrcDepth orelse 3,
+    };
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -119,42 +229,54 @@ pub fn main() !void {
     var cwd = try std.fs.cwd().openDir(".", .{ .iterate = true });
     defer cwd.close();
 
-    // TODO: Make it configurable
-    const maxDepth = 3;
+    const config = try parseConfigFromArgs(allocator);
+    defer config.deinit(allocator);
 
     // TODO: Ignore .gitignore files
     var dirWalker = try walker.walk(allocator, cwd);
     defer dirWalker.deinit();
     while (try dirWalker.next()) |entry| next: {
-        if (entry.kind == .directory and std.mem.startsWith(u8, entry.basename, ".")) {
-            // NOTE: Revisit blocking all the hidden files
-            @branchHint(.unlikely);
-            dirWalker.skip();
-            continue;
-        } else if (entry.kind == .directory and dirWalker.depth() > maxDepth and std.mem.indexOf(u8, entry.path, "src") == null) {
-            // PERF: This check avoids unnecessary traversals on non-code paths in the directory structure
-            dirWalker.skip();
-            continue;
-        } else if (entry.kind == .file) {
-            @branchHint(.likely);
-            const extension = std.fs.path.extension(entry.basename);
-
-            // PERF: Avoid checking files that are not actual source files, based on extensions
-            check: {
-                for (extensions) |ext| {
-                    if (std.mem.eql(u8, ext, extension)) {
-                        break :check;
-                    }
+        switch (entry.kind) {
+            .directory => {
+                if (std.mem.startsWith(u8, entry.basename, ".")) {
+                    // NOTE: Revisit blocking all the hidden files
+                    @branchHint(.unlikely);
+                    dirWalker.skip();
+                    continue;
+                } else if (config.maxSrcDepth > 0 and dirWalker.depth() > config.maxSrcDepth and std.mem.indexOf(u8, entry.path, "src") == null) {
+                    // PERF: This check avoids unnecessary traversals on non-code paths in the directory structure
+                    dirWalker.skip();
+                    continue;
+                } else if (config.maxDepth > 0 and dirWalker.depth() > config.maxDepth) {
+                    // PERF: This check avoids unnecessary traversals on too nested paths in the directory structure
+                    dirWalker.skip();
+                    continue;
                 }
-                break :next;
-            }
+            },
+            .file => {
+                @branchHint(.likely);
+                const extension = std.fs.path.extension(entry.basename);
+                // NOTE: when the file is either `.gitignore` or `something.`, the size will be smaller than 2 ("" or "." respectively)
+                if (extension.len < 2) break :next;
 
-            defer _ = arena.reset(.retain_capacity);
+                // PERF: Avoid checking files that are not actual source files, based on extensions
+                check: {
+                    for (config.fileTypes) |ext| {
+                        if (std.mem.eql(u8, ext, extension[1..])) {
+                            break :check;
+                        }
+                    }
+                    break :next;
+                }
 
-            const file = try entry.dir.openFile(entry.basename, .{ .mode = .read_only });
-            defer file.close();
+                defer _ = arena.reset(.retain_capacity);
 
-            try readTodos(arenaAllocator, entry.path, file);
+                const file = try entry.dir.openFile(entry.basename, .{ .mode = .read_only });
+                defer file.close();
+
+                try readTodos(arenaAllocator, entry.path, file, config);
+            },
+            else => {},
         }
     }
 }
