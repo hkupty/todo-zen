0185c2ee1823e312be4424d7b875967811c8c9bd
Henry Kupty <hkupty@gmail.com>
2025-08-18 23:14:02 +0200

    refactor: Move config out + threshold

---
+++ src/config.zig
@@ -0,0 +1,135 @@
+const std = @import("std");
+
+const stdout = std.io.getStdOut();
+
+const variants = [_][]const u8{ "TODO", "HACK", "FIX", "FIXME" };
+
+// TODO: Enable different comment prefixes for different file types
+const extensions = [_][]const u8{
+    "zig",
+    "kt",
+    "java",
+    "go",
+};
+
+const help =
+    \\todo-zen [options]
+    \\
+    \\Options:
+    \\     -h --help            Shows this help text
+    \\     -d --max-depth       Maximum traversal depth.                                [default: 8]
+    \\                          Set to 0 to disable.
+    \\     -D --max-src-depth   Maximum depth for a `src/` folder.                      [default: 3]
+    \\                          Set to 0 to disable.
+    \\     -t --threshold       Maximum number of todos. Will exit 1 if exceeds value.  [default: 16]
+    \\                          Set to 0 to disable.
+    \\     -m --markers         Comment markers to look for in the comments.            [default: TODO,HACK,FIX,FIXME]
+    \\     -x --extensions      File extensions to be considered during search.         [default: zig,java,kt,go]
+    \\
+;
+
+markers: [][]const u8,
+maxDepth: usize,
+maxSrcDepth: usize,
+threshold: usize,
+fileTypes: [][]const u8,
+
+const Config = @This();
+
+pub fn deinit(self: *const Config, allocator: std.mem.Allocator) void {
+    for (self.markers) |marker| {
+        allocator.free(marker);
+    }
+
+    for (self.fileTypes) |ft| {
+        allocator.free(ft);
+    }
+    allocator.free(self.markers);
+    allocator.free(self.fileTypes);
+}
+
+pub fn parseConfigFromArgs(allocator: std.mem.Allocator) !Config {
+    var args = std.process.args();
+    defer args.deinit();
+    _ = args.skip();
+
+    var cursor: ?[]const u8 = null;
+    var markers: ?[][]const u8 = null;
+    var filetypes: ?[][]const u8 = null;
+    var maxDepth: ?usize = null;
+    var maxSrcDepth: ?usize = null;
+    var threshold: ?usize = null;
+    while (args.next()) |arg| {
+        if (cursor) |flag| {
+            if (std.mem.eql(u8, "-m", flag) or std.mem.eql(u8, "--markers", flag)) {
+                const max = std.mem.count(u8, arg, ",") + 1;
+                markers = try allocator.alloc([]const u8, max);
+
+                var splitIter = std.mem.splitScalar(u8, arg, ',');
+                var ix: usize = 0;
+                while (splitIter.next()) |nxt| : (ix += 1) {
+                    const marker = allocator.dupe(u8, nxt) catch |err| {
+                        std.log.err(
+                            "Failed to allocate more memory. Will try to operate on best effort. {any}",
+                            .{err},
+                        );
+                        break;
+                    };
+                    markers.?[ix] = marker;
+                }
+            } else if (std.mem.eql(u8, "-x", flag) or std.mem.eql(u8, "--extensions", flag)) {
+                const max = std.mem.count(u8, arg, ",") + 1;
+                filetypes = try allocator.alloc([]const u8, max);
+
+                var splitIter = std.mem.splitScalar(u8, arg, ',');
+                var ix: usize = 0;
+                while (splitIter.next()) |nxt| : (ix += 1) {
+                    const ft = allocator.dupe(u8, nxt) catch |err| {
+                        std.log.err(
+                            "Failed to allocate more memory. Will try to operate on best effort. {any}",
+                            .{err},
+                        );
+                        break;
+                    };
+                    filetypes.?[ix] = ft;
+                }
+            } else if (std.mem.eql(u8, "-d", flag) or std.mem.eql(u8, "--max-depth", flag)) {
+                maxDepth = try std.fmt.parseInt(usize, arg, 10);
+            } else if (std.mem.eql(u8, "-D", flag) or std.mem.eql(u8, "--max-src-depth", flag)) {
+                maxSrcDepth = try std.fmt.parseInt(usize, arg, 10);
+            } else if (std.mem.eql(u8, "-t", flag) or std.mem.eql(u8, "--threshold", flag)) {
+                threshold = try std.fmt.parseInt(usize, arg, 10);
+            }
+            cursor = null;
+        } else {
+            if (std.mem.eql(u8, "-h", arg) or std.mem.eql(u8, "--help", arg)) {
+                _ = try stdout.writeAll(help);
+                std.process.exit(0);
+            }
+
+            cursor = arg;
+        }
+    }
+
+    if (markers == null) {
+        markers = try allocator.alloc([]const u8, variants.len);
+        for (variants, 0..) |marker, ix| {
+            markers.?[ix] = try allocator.dupe(u8, marker);
+        }
+    }
+
+    if (filetypes == null) {
+        filetypes = try allocator.alloc([]const u8, extensions.len);
+        for (extensions, 0..) |ft, ix| {
+            filetypes.?[ix] = try allocator.dupe(u8, ft);
+        }
+    }
+
+    return .{
+        .markers = markers.?,
+        .fileTypes = filetypes.?,
+        .maxDepth = maxDepth orelse 8,
+        .maxSrcDepth = maxSrcDepth orelse 3,
+        .threshold = threshold orelse 16,
+    };
+}
--- src/main.zig
+++ src/main.zig
@@ -1,48 +1,8 @@
 const std = @import("std");
 const walker = @import("walker.zig");
-
-const variants = [_][]const u8{ "TODO", "HACK", "FIX", "FIXME" };
-
-// TODO: Enable different comment prefixes for different file types
-const extensions = [_][]const u8{
-    "zig",
-    "kt",
-    "java",
-    "go",
-};
+const Config = @import("config.zig");
 
 const stdout = std.io.getStdOut();
-
-const help =
-    \\todo-zen [options]
-    \\
-    \\Options:
-    \\     -h --help            Shows this help text
-    \\     -d --max-depth       Maximum traversal depth. Set to 0 to disable            [default: 8]
-    \\     -D --max-src-depth   Maximum depth for a `src/` folder. Set to 0 to disable  [default: 3]
-    \\     -m --markers         Comment markers to look for in the comments.            [default: TODO,HACK,FIX,FIXME]
-    \\     -x --extensions      File extensions to be considered during search.         [default: zig,java,kt,go]
-    \\
-;
-
-const Config = struct {
-    markers: [][]const u8,
-    maxDepth: usize,
-    maxSrcDepth: usize,
-    fileTypes: [][]const u8,
-
-    pub fn deinit(self: *const Config, allocator: std.mem.Allocator) void {
-        for (self.markers) |marker| {
-            allocator.free(marker);
-        }
-
-        for (self.fileTypes) |ft| {
-            allocator.free(ft);
-        }
-        allocator.free(self.markers);
-        allocator.free(self.fileTypes);
-    }
-};
 
 const Match = struct {
     text: []u8,
@@ -107,7 +67,7 @@ const TodoIterator = struct {
     }
 };
 
-fn readTodos(allocator: std.mem.Allocator, identifier: []const u8, file: std.fs.File, config: Config) !void {
+fn readTodos(allocator: std.mem.Allocator, identifier: []const u8, file: std.fs.File, config: Config) !usize {
     var iterator = TodoIterator{
         .reader = file.reader(),
         .allocator = allocator,
@@ -118,8 +78,9 @@ fn readTodos(allocator: std.mem.Allocator, identifier: []const u8, file: std.fs.
 
     lineBuilder.appendSliceAssumeCapacity(identifier);
     lineBuilder.appendAssumeCapacity(':');
+    var count: usize = 0;
 
-    while (try iterator.readTODOComments(config)) |line| {
+    while (try iterator.readTODOComments(config)) |line| : (count += 1) {
         lineBuilder.shrinkRetainingCapacity(identifier.len + 1);
         const linenumber = try std.fmt.allocPrint(allocator, "{d}", .{line.linenumber});
         const column = try std.fmt.allocPrint(allocator, "{d}", .{line.column});
@@ -133,88 +94,8 @@ fn readTodos(allocator: std.mem.Allocator, identifier: []const u8, file: std.fs.
 
         _ = try stdout.write(lineBuilder.items);
     }
-}
 
-pub fn parseConfigFromArgs(allocator: std.mem.Allocator) !Config {
-    var args = std.process.args();
-    defer args.deinit();
-    _ = args.skip();
-
-    var cursor: ?[]const u8 = null;
-    var markers: ?[][]const u8 = null;
-    var filetypes: ?[][]const u8 = null;
-    var maxDepth: ?usize = null;
-    var maxSrcDepth: ?usize = null;
-    while (args.next()) |arg| {
-        if (cursor) |flag| {
-            if (std.mem.eql(u8, "-m", flag) or std.mem.eql(u8, "--markers", flag)) {
-                const max = std.mem.count(u8, arg, ",") + 1;
-                markers = try allocator.alloc([]const u8, max);
-
-                var splitIter = std.mem.splitScalar(u8, arg, ',');
-                var ix: usize = 0;
-                while (splitIter.next()) |nxt| : (ix += 1) {
-                    const marker = allocator.dupe(u8, nxt) catch |err| {
-                        std.log.err(
-                            "Failed to allocate more memory. Will try to operate on best effort. {any}",
-                            .{err},
-                        );
-                        break;
-                    };
-                    markers.?[ix] = marker;
-                }
-            } else if (std.mem.eql(u8, "-x", flag) or std.mem.eql(u8, "--extensions", flag)) {
-                const max = std.mem.count(u8, arg, ",") + 1;
-                filetypes = try allocator.alloc([]const u8, max);
-
-                var splitIter = std.mem.splitScalar(u8, arg, ',');
-                var ix: usize = 0;
-                while (splitIter.next()) |nxt| : (ix += 1) {
-                    const ft = allocator.dupe(u8, nxt) catch |err| {
-                        std.log.err(
-                            "Failed to allocate more memory. Will try to operate on best effort. {any}",
-                            .{err},
-                        );
-                        break;
-                    };
-                    filetypes.?[ix] = ft;
-                }
-            } else if (std.mem.eql(u8, "-d", flag) or std.mem.eql(u8, "--max-depth", flag)) {
-                maxDepth = try std.fmt.parseInt(usize, arg, 10);
-            } else if (std.mem.eql(u8, "-D", flag) or std.mem.eql(u8, "--max-src-depth", flag)) {
-                maxSrcDepth = try std.fmt.parseInt(usize, arg, 10);
-            }
-            cursor = null;
-        } else {
-            if (std.mem.eql(u8, "-h", arg) or std.mem.eql(u8, "--help", arg)) {
-                _ = try stdout.writeAll(help);
-                std.process.exit(0);
-            }
-
-            cursor = arg;
-        }
-    }
-
-    if (markers == null) {
-        markers = try allocator.alloc([]const u8, variants.len);
-        for (variants, 0..) |marker, ix| {
-            markers.?[ix] = try allocator.dupe(u8, marker);
-        }
-    }
-
-    if (filetypes == null) {
-        filetypes = try allocator.alloc([]const u8, extensions.len);
-        for (extensions, 0..) |ft, ix| {
-            filetypes.?[ix] = try allocator.dupe(u8, ft);
-        }
-    }
-
-    return .{
-        .markers = markers.?,
-        .fileTypes = filetypes.?,
-        .maxDepth = maxDepth orelse 8,
-        .maxSrcDepth = maxSrcDepth orelse 3,
-    };
+    return count;
 }
 
 pub fn main() !void {
@@ -229,12 +110,13 @@ pub fn main() !void {
     var cwd = try std.fs.cwd().openDir(".", .{ .iterate = true });
     defer cwd.close();
 
-    const config = try parseConfigFromArgs(allocator);
+    const config = try Config.parseConfigFromArgs(allocator);
     defer config.deinit(allocator);
 
     // TODO: Ignore .gitignore files
     var dirWalker = try walker.walk(allocator, cwd);
     defer dirWalker.deinit();
+    var count: usize = 0;
     while (try dirWalker.next()) |entry| next: {
         switch (entry.kind) {
             .directory => {
@@ -274,9 +156,13 @@ pub fn main() !void {
                 const file = try entry.dir.openFile(entry.basename, .{ .mode = .read_only });
                 defer file.close();
 
-                try readTodos(arenaAllocator, entry.path, file, config);
+                count += try readTodos(arenaAllocator, entry.path, file, config);
             },
             else => {},
         }
+    }
+
+    if (config.threshold > 0 and count > config.threshold) {
+        std.process.exit(1);
     }
 }
